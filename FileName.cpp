0e66c186bc4262bd352e8cab148926570f3af33b
Lukasz <lukaszgerst@gmail.com>
2023-06-13 21:04:02 +0200

    mam nadzieje ze finalny

--- FileName.cpp
+++ FileName.cpp
@@ -11,7 +11,7 @@ using namespace std;
 
 class Produkt
 {
-public:
+protected:
 	string nazwa;
 	int kod_produktu;
 	int ilosc;
@@ -22,16 +22,19 @@ public:
 	friend vector<Produkt> wczytaj_z_pliku(Produkt &p);
 	friend void wczytaj_do_pliku(vector<Produkt>p);
 	friend vector<Produkt> usun_produkt(vector<Produkt> produkty);
-
+	friend struct porownanie;
+	friend void wyswietl_produkty(vector<Produkt> produkty);
+	friend vector<Produkt> dodaj_zamowienie(vector<Produkt> produkty);
+	friend vector<Produkt> dodaj_dostawe(vector<Produkt> produkty);
 	Produkt(string , int , int , float);
 };
 
 class Dostawa
 {
-public:
-	Produkt produkt;
+protected:
 	int ilosc_przyjeta;
-
+	Produkt produkt;
+public:
 	Dostawa(Produkt prod, int ilosc);
 };
 
@@ -40,9 +43,10 @@ Dostawa::Dostawa(Produkt prod, int ilosc) : produkt(prod), ilosc_przyjeta(ilosc)
 
 class Zamowienie
 {
-public:
+protected:
 	Produkt produkt;
 	int ilosc_zamowiona;
+public:
 	Zamowienie(Produkt prod, int ilosc);
 };
 
@@ -52,7 +56,7 @@ void wyswietl_produkty(vector<Produkt> produkty);
 
 Produkt::Produkt(string naz = "domyslny", int kod = 0, int il = 0, float cen = 0) : nazwa(naz), kod_produktu(kod), ilosc(il), cena(cen) {};
 
-struct less_than_key
+struct porownanie
 {
 	inline bool operator() (const Produkt& obj1, const Produkt& obj2)
 	{
@@ -104,13 +108,13 @@ void wczytaj_do_pliku(vector<Produkt> p)
 vector<Produkt> dodaj_produkt(vector<Produkt> produkty) 
 {
 	system("cls");
-	string nazwa;
+	string nazwa{};
 	int kod, ilosc;
 	float cena;
 
 	cout<< endl << "WprowadŸ nazwê nowego produktu: ";
-	cin.ignore();
 	cin.clear();
+	cin.ignore();
 	getline(cin, nazwa);
 	cout << "Kod produktu: ";
 	cin >> kod;
@@ -146,17 +150,24 @@ vector<Produkt> dodaj_produkt(vector<Produkt> produkty)
 
 	cout << "Cena produktu: ";
 	cin >> cena;
+	while (cin.fail())
+	{
+		cout << "Cena musi byæ liczb¹, podaj jeszcze raz: ";
+		cin.clear();
+		cin.ignore(256, '\n');
+		cin >> cena;
+	}
 	cout << endl;
 	Produkt p(nazwa, kod, ilosc, cena);
 	produkty.push_back(p); // zapisuje produkt utworzony przez uzytkownika do wektora objektow Produkt
-	cout << "POMYSLNIE DODANO PRODUKT!" << endl << endl;
+	cout << "POMYŒLNIE DODANO PRODUKT!" << endl << endl;
 
-	string wybor{};
-	cout << "Czy chcesz dodaæ kolejny produkt? 1 - tak, enter - powrót do menu" << endl;
+	char wybor{};
+	cout << "Czy chcesz dodaæ kolejny produkt? 1 - tak, dowolny klawisz zatwierdzony enterem - powrót do menu" << endl;
 	cin.ignore();
-	getline(cin, wybor);
-	if (wybor[0] == 49) produkty = dodaj_produkt(produkty);
-
+	cin >> wybor;
+	if (wybor == '1') produkty = dodaj_produkt(produkty);
+	
 	return produkty;
 }
 
@@ -219,6 +230,8 @@ vector<Produkt> edytuj_produkt(vector<Produkt>p)
 	{
 		if (p[i].kod_produktu == kod)
 		{
+			cin.clear();
+			cin.ignore();
 			cout << "Wprowadz nowa nazwe: ";
 			cin >> nazwa;
 			cout << "Wprowadz nowa ilosc: ";
@@ -229,7 +242,7 @@ vector<Produkt> edytuj_produkt(vector<Produkt>p)
 			p[i].ilosc = ilosc;
 			p[i].cena = cena;
 			licznik++;
-			cout << "POMYSLNIE EDYTOWANO PRODUKT!" << endl;
+			cout << endl << "POMYSLNIE EDYTOWANO PRODUKT!" << endl << endl;
 		}
 	}
 	if (licznik == 0) cout << "Nie znaleziono takiego produktu" << endl;
@@ -238,16 +251,16 @@ vector<Produkt> edytuj_produkt(vector<Produkt>p)
 	cout << "Czy chcesz edytowaæ kolejny produkt? 1 - tak, enter - powrót do menu" << endl;
 	cin.ignore();
 	getline(cin, wybor);
-	if (wybor[0] = 49) p = edytuj_produkt(p);
+	if (wybor[0] == 49) p = edytuj_produkt(p);
 
 	return p;
 }
 
 void wyswietl_produkty(vector<Produkt> produkty)
 {
-	sort(produkty.begin(), produkty.end(), less_than_key());
+	sort(produkty.begin(), produkty.end(), porownanie());
 	system("cls");
-	cout << setw(15) << "Nazwa produktu" << setw(20) << "Kod produktu" << setw(20) << "Stan magazynowy" << setw(20) << "Cena" << endl;
+	cout << setw(15) << "Nazwa produktu" << setw(20) << "Kod produktu" << setw(20) << "Stan magazynowy" << setw(20) << "Cena" << endl << endl;
 	for (int i = 0; i < produkty.size(); i++)
 	{
 		cout << setw(15) << produkty[i].nazwa << setw(20) << produkty[i].kod_produktu << setw(20) << produkty[i].ilosc << setw(20) << produkty[i].cena << endl;
@@ -258,22 +271,35 @@ void wyswietl_produkty(vector<Produkt> produkty)
 void wyswietl_menu()
 {
 	system("cls");
-	cout << "Witaj w systemie zarz¹dzania twoim sklepem internetowym!!!" << endl << endl;
-	cout << "Wybierz dostepn¹ opcjê: " << endl;
+	cout << "----------------------------------------------------------------------" << endl;
+	cout << "----------------------------------------------------------------------" << endl;
+	cout << "------Witaj w systemie zarz¹dzania twoim sklepem internetowym!!!------" << endl;
+	cout << "----------------------------------------------------------------------" << endl;
+	cout << "----------------------------------------------------------------------" << endl << endl;
 	cout << "1. Dodaj nowy produkt" << endl;
 	cout << "2. Usuñ produkt" << endl;
 	cout << "3. Edytuj produkt" << endl;
-	cout << "4. Wyœwietl wszystkie produkty" << endl;
-	cout << "Aby opuœciæ program i zapisaæ zmiany, nale¿y wpisaæ inn¹ liczbê ni¿ jest dostêpna" << endl << endl;
+	cout << "4. Dodaj produkt z dostawy" << endl;
+	cout << "5. Dodaj zamówienie klienta" << endl;
+	cout << "6. Wyœwietl wszystkie produkty" << endl << endl;
+	cout << "Wpisanie innej opcji ni¿ jest dostêpna powoduje opuszczenie programu i zapisanie zmian" << endl << endl;
+	cout << "Wybierz opcjê (1-6): ";
 }
 
 vector<Produkt> dodaj_dostawe(vector<Produkt> produkty)
 {
 	vector<Dostawa> dostawa;
-	int kod, ilosc_przyjeta, wybor;
-
+	int kod, ilosc_przyjeta;
+	//char wybor;
 	cout << "Podaj kod produktu, który chcesz przyj¹c na magazyn: ";
 	cin >> kod;
+	while (cin.fail())
+	{
+		cout << "Kod musi byæ liczb¹, podaj jeszcze raz: ";
+		cin.clear();
+		cin.ignore(256, '\n');
+		cin >> kod;
+	}
 
 	bool produkt_znaleziony = false;
 	for (int i = 0; i < produkty.size(); i++)
@@ -283,6 +309,13 @@ vector<Produkt> dodaj_dostawe(vector<Produkt> produkty)
 			produkt_znaleziony = true;
 			cout << "Podaj iloœæ przyjêtego produktu: ";
 			cin >> ilosc_przyjeta;
+			while (cin.fail())
+			{
+				cout << "Iloœæ musi byæ liczb¹, podaj jeszcze raz: ";
+				cin.clear();
+				cin.ignore(256, '\n');
+				cin >> ilosc_przyjeta;
+			}
 
 			Dostawa zamowienie(produkty[i], ilosc_przyjeta);
 			produkty[i].ilosc += ilosc_przyjeta;
@@ -293,34 +326,32 @@ vector<Produkt> dodaj_dostawe(vector<Produkt> produkty)
 	if (!produkt_znaleziony)
 		cout << "Nie znaleziono takiego produktu" << endl;
 
-	cout << endl << "Czy chcesz przyj¹æ nastêpny produkt z dostawy?" << endl << "1 - tak, 2 - nie" << endl;
-	cin >> wybor;
-	cout << endl;
-	switch (wybor)
-	{
-	case 1:
-		produkty = dodaj_dostawe(produkty);
-		break;
-	case 2:
-		break;
-	default:
-		cout << "Nie ma takiej opcji, powrót do menu" << endl;
-	}
+	string wybor{};
+	cout << "Czy chcesz przyj¹æ kolejny produkt? 1 - tak, enter - powrót do menu" << endl;
+	cin.ignore();
+	getline(cin, wybor);
+	if (wybor[0] == 49) produkty = edytuj_produkt(produkty);
 
 	return produkty;
-
-
 }
-
 
 //ZAMOWIENIE
 vector<Produkt> dodaj_zamowienie(vector<Produkt> produkty)
 {
-	int kod, ilosc_zamowiona, wybor;
+	int kod, ilosc_zamowiona;
+	char wybor;
 	vector<Zamowienie> zamowienie;
 
 	cout << "Podaj kod produktu, który zostal zamówiony: " << endl;
 	cin >> kod;
+
+	while (cin.fail())
+	{
+		cout << "Kod musi byæ liczb¹, podaj jeszcze raz: ";
+		cin.clear();
+		cin.ignore(256, '\n');
+		cin >> kod;
+	}
 
 	bool produkt_znaleziony = false;
 	for (int i = 0; i < produkty.size(); i++)
@@ -330,32 +361,41 @@ vector<Produkt> dodaj_zamowienie(vector<Produkt> produkty)
 			produkt_znaleziony = true;
 			cout << "Podaj iloœæ zamówionego produktu: " << endl;
 			cin >> ilosc_zamowiona;
+			while (cin.fail())
+			{
+				cout << "Iloœæ musi byæ liczb¹, podaj jeszcze raz: ";
+				cin.clear();
+				cin.ignore(256, '\n');
+				cin >> ilosc_zamowiona;
+			}
 
 			Zamowienie zamowienie(produkty[i], ilosc_zamowiona);
 			produkty[i].ilosc -= ilosc_zamowiona;
 			if (produkty[i].ilosc < 0)
 			{
 				cout << "OSTRZE¯ENIE! Brak wystarczaj¹cej iloœci produktu na magazynie, czy chcesz potwierdziæ operacjê?" << endl << "1 - tak, 2 - nie" << endl;
-				cin >> wybor;
+				while (true)
+				{
+					cin >> wybor;
+
+					if (wybor == '1' || wybor == '2')
+					{
+						break;
+					}
+					else
+					{
+						cout << "nieprawidlowy wybor, wybierz opcjê 1 lub 2: ";
+					}
+				}
+
 				switch (wybor)
+
 				{
 				case 1:
 					break;
 				case 2:
 					produkty[i].ilosc += ilosc_zamowiona;
-					/*cout << "Czy chcesz dodaæ nastêpny produkt do zamówienia?" << endl << "1 - tak, 2 - nie" << endl;
-					cin >> wybor;
 
-					switch (wybor)
-					{
-					case 1:
-						produkty = dodaj_zamowienie(produkty);
-						break;
-					case 2:
-						break;
-					default:
-						cout << "Nie ma takiej opcji, powrot do menu";
-					}*/
 					break;
 				default:
 					cout << "Nie ma takiej opcji, powrot do menu";
@@ -364,12 +404,23 @@ vector<Produkt> dodaj_zamowienie(vector<Produkt> produkty)
 			break;
 		}
 	}
-
 	if (!produkt_znaleziony)
 		cout << "Nie znaleziono takiego produktu" << endl;
 
 	cout << "Czy chcesz dodac nastepny produkt do zamowienia?" << endl << "1 - tak, 2 - nie" << endl;
-	cin >> wybor;
+
+	while (true)
+	{
+		cin >> wybor;
+		if (wybor == '1' || wybor == '2')
+		{
+			break;
+		}
+		else
+		{
+			cout << "nieprawidlowy wybor, wybierz opcjê 1 lub 2: ";
+		}
+	}
 
 	switch (wybor)
 	{
@@ -379,12 +430,78 @@ vector<Produkt> dodaj_zamowienie(vector<Produkt> produkty)
 	case 2:
 		break;
 	default:
-		cout << "Nie ma takiej opcji, powrot do menu";
+		cout << "wybierz opcjê 1 lub2 ";
 	}
-
 	return produkty;
+}
 
+bool logowanie()
+{
+	string nazwa, haslo, uzytkownik, pass;
+	cout << "WprowadŸ nazwê u¿ytkownika: \n";
+	cin >> nazwa;
+	cout << "Podaj has³o: \n";
+	cin >> haslo;
 
+	ifstream czytaj(nazwa + "login.txt");
+	getline(czytaj, uzytkownik);
+	getline(czytaj, pass);
+
+	if (uzytkownik == nazwa && pass == haslo)
+	{
+		return true;
+
+	}
+	else
+	{
+		return false;
+	}
+}
+
+int login()
+{
+	char wybor;
+
+	cout << endl << endl;
+	cout << setw(69) << "Wybierz opcjê:" << endl;
+	cout << endl << setw(65) << "1:Zaloguj siê";
+	cout << endl << setw(69) << "2:Zarejestruj siê" << endl;
+
+	cin >> wybor;
+	system("cls");
+	if (wybor == '2')
+	{
+		string nazwa, haslo;
+		cout << "Wybierz nazwê u¿ytkownika: ";
+		cin >> nazwa;
+		cout << "Wybierz has³o: ";
+		cin >> haslo;
+		cout << "pomyœlnie zarejestrowano";
+		ofstream plik;
+		plik.open(nazwa + "login.txt");
+		plik << nazwa << endl << haslo;
+		plik.close();
+		system("cls");
+		login();
+	}
+	else if (wybor == '1')
+	{
+		bool status = logowanie();
+		if (!status)
+		{
+			cout << "Wprowadzono nieprawid³owe dane, spróbuj ponownie" << endl;
+			system("PAUSE");
+			cout << endl;
+			system("cls");
+			login();
+		}
+	}
+	else
+	{
+		login();
+		return 0;
+		system("cls");
+	}
 }
 
 int main()
@@ -394,23 +511,28 @@ int main()
 	vector<Produkt> produkty;
 	produkty = wczytaj_z_pliku(p1);
 	
-	int wybor;
+	login();
+
+	char wybor;
 	do
 	{
 		wyswietl_menu();
-		cout << "Wybierz opcjê (1-4): ";
 		cin >> wybor;
 		switch (wybor)
 		{
-		case 1: produkty = dodaj_produkt(produkty); break;
-		case 2: produkty = usun_produkt(produkty); break;
-		case 3: produkty = edytuj_produkt(produkty); break;
-		case 4: wyswietl_produkty(produkty); break;
+		case '1': produkty = dodaj_produkt(produkty); break;
+		case '2': produkty = usun_produkt(produkty); break;
+		case '3': produkty = edytuj_produkt(produkty); break;
+		case '4': produkty = dodaj_dostawe(produkty); break;
+		case '5': produkty = dodaj_zamowienie(produkty); break;
+		case '6': wyswietl_produkty(produkty); break;
 		}
-	} while (wybor > 0 && wybor < 5);
-	/*wyswietl_produkty(produkty);
-	produkty = dodaj_zamowienie(produkty);
-	wyswietl_produkty(produkty);*/
+		if (wybor <= '0' || wybor >= '7')
+		{
+			cout << endl <<"OPUSZCZANIE PROGRAMU, ZMIANY ZOSTA£Y ZAPISANE!" << endl;
+			break;
+		}
+	} while (1);
 
 	wczytaj_do_pliku(produkty);
 }
